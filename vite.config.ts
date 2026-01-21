8930d1012411e66d327cfe3f26111dab76d16e7b
abbaty <abbaty48@gmail.com>
2026-01-21 21:04:14 +0100

    Configure server options in vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,19 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+    proxy: {
+      '/api': {
+        secure: false,
+        changeOrigin: true,
+        target: 'http://localhost:8000',
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      }
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
