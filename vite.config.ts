1e102b70045f2dd59335eb1d505e86c246ccabdd
Abbatyya <abbaty48@gmail.com>
2026-01-21 21:33:35 +0100

    Add allowed hosts to Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   server: {
     host: true,
     strictPort: true,
+    allowedHosts: ["localhost", "library-repository-3vai.onrender.com"],
     port: process.env.PORT ? Number(process.env.PORT) : 3000,
     proxy: {
       '/api': {
